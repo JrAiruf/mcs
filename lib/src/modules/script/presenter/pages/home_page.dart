bf878fdba77aad8fb42281ff32e9543f96eb57d9
Júnior Oliveira <jos.pd2019@gmail.com>
2024-01-18 08:14:56 -0300

    Deleting unused static Scripts List

--- lib/src/modules/script/presenter/pages/home_page.dart
+++ lib/src/modules/script/presenter/pages/home_page.dart
@@ -81,19 +81,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-final _scriptNames = [
-  "Script 1",
-  "Script 2",
-  "Script 3",
-  "Script 4",
-  "Script 5",
-  "Script 6",
-  "Script 7",
-  "Script 8",
-  "Script 9",
-  "Script 10",
-  "Script 11",
-  "Script 12",
-  "Script 13",
-];
